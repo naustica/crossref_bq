89aff923701290be89ab531c6c7c97279d832040
Nick <37081556+naustica@users.noreply.github.com>
2021-04-17 12:53:58 +0200

    add sql scripts

---
+++ transform.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -p medium
+#SBATCH -C scratch
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 16
+#SBATCH -t 10:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=nick.haupka@sub.uni-goettingen.de
+
+cd /scratch/users/haupka/transform && ls -1 * | xargs -P 16 gzip
