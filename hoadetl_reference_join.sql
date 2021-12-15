5d05388555e50353e7ff782c8af7f7dd0465f10b
Nick Haupka <nick.haupka@outlook.de>
2021-12-15 13:17:57 +0100

    update query

--- hoadetl_reference_join.sql
+++ hoadetl_reference_join.sql
@@ -1,13 +1,14 @@
 SELECT cr_derived.*, cr_reference.reference
-FROM `api-project-764811344545.cr_instant.snapshot` AS cr_derived
+FROM `subugoe-collaborative.cr_instant.snapshot` AS cr_derived
 LEFT OUTER JOIN (SELECT doi, reference
                  FROM (
                     SELECT y.doi,
                            ARRAY_AGG(STRUCT(rec.key,
                                             rec.doi,
                                             rec.doi_asserted_by,
+                                            rec.unstructured,
                                             rec.journal_title)) AS reference,
-                    FROM `api-project-764811344545.cr_instant.snapshot_complete` AS y
+                    FROM `subugoe-collaborative.cr_instant.snapshot_complete` AS y
                     LEFT JOIN UNNEST(reference) AS rec
                     GROUP BY doi
                   )) AS cr_reference
